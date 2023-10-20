31a1c11be8a8a1d4a25feeb311141958a53bbbf9
Brandon Perfetti <brandon@brandonperfetti.com>
2023-10-20 12:46:55 -0700

    Temp revert auth handler.

--- src/pages/api/twitter/auth.ts
+++ src/pages/api/twitter/auth.ts
@@ -52,40 +52,6 @@ export const fetchUserData = async (accessToken: string) => {
 	}
 }
 
-// export default async function handler(
-// 	req: NextApiRequest,
-// 	res: NextApiResponse,
-// ) {
-// 	const { code } = req.body
-// 	try {
-// 		const tokenResponse = await fetchUserToken(code)
-// 		const accessToken = tokenResponse.access_token
-
-// 		if (accessToken) {
-// 			const userDataResponse = await fetchUserData(accessToken)
-// 			const userCredentials = { ...tokenResponse, ...userDataResponse }
-// 			console.log('CR>>', {
-// 				accessToken: userCredentials.access_token,
-// 				_id: userCredentials.data.id,
-// 				username: userCredentials.data.username,
-// 			})
-
-// 			const { data, error } = await supabase.from('users').insert({
-// 				id: userCredentials.data.id,
-// 				accessToken: userCredentials.access_token,
-// 				username: userCredentials.data.username,
-// 			})
-// 			if (!error) {
-// 				return res.status(200).json(userCredentials)
-// 			} else {
-// 				return res.status(400).json({ error })
-// 			}
-// 		}
-// 	} catch (err) {
-// 		return res.status(400).json({ err })
-// 	}
-// }
-
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse,
@@ -98,52 +64,86 @@ export default async function handler(
 		if (accessToken) {
 			const userDataResponse = await fetchUserData(accessToken)
 			const userCredentials = { ...tokenResponse, ...userDataResponse }
+			console.log('CR>>', {
+				accessToken: userCredentials.access_token,
+				_id: userCredentials.data.id,
+				username: userCredentials.data.username,
+			})
 
-			// Check if a user with the same username already exists
-			const { data: existingUser, error: existingUserError } = await supabase
-				.from('users')
-				.select('*')
-				.eq('username', userCredentials.data.username)
-				.single() // Use single() if username is unique and at most one record should match
-
-			if (existingUserError) {
-				// Handle error checking for existing user
-				return res.status(400).json({ error: existingUserError.message })
-			}
-
-			if (existingUser) {
-				// User with the same username already exists
-				// Decide whether to update existing user or return an error
-				// For example, to update the existing user:
-				const { error: updateError } = await supabase
-					.from('users')
-					.update({
-						accessToken: userCredentials.access_token,
-						// ... other fields to update
-					})
-					.eq('username', userCredentials.data.username)
-
-				if (updateError) {
-					return res.status(400).json({ error: updateError.message })
-				}
-
+			const { data, error } = await supabase.from('users').insert({
+				id: userCredentials.data.id,
+				accessToken: userCredentials.access_token,
+				username: userCredentials.data.username,
+			})
+			if (!error) {
 				return res.status(200).json(userCredentials)
 			} else {
-				// No user with the same username found, create new user
-				const { data, error } = await supabase.from('users').insert({
-					id: userCredentials.data.id,
-					accessToken: userCredentials.access_token,
-					username: userCredentials.data.username,
-				})
-
-				if (error) {
-					return res.status(400).json({ error })
-				}
-
-				return res.status(200).json(userCredentials)
+				return res.status(400).json({ error })
 			}
 		}
 	} catch (err) {
 		return res.status(400).json({ err })
 	}
 }
+
+// export default async function handler(
+// 	req: NextApiRequest,
+// 	res: NextApiResponse,
+// ) {
+// 	const { code } = req.body
+// 	try {
+// 		const tokenResponse = await fetchUserToken(code)
+// 		const accessToken = tokenResponse.access_token
+
+// 		if (accessToken) {
+// 			const userDataResponse = await fetchUserData(accessToken)
+// 			const userCredentials = { ...tokenResponse, ...userDataResponse }
+
+// 			// Check if a user with the same username already exists
+// 			const { data: existingUser, error: existingUserError } = await supabase
+// 				.from('users')
+// 				.select('*')
+// 				.eq('username', userCredentials.data.username)
+// 				.single() // Use single() if username is unique and at most one record should match
+
+// 			if (existingUserError) {
+// 				// Handle error checking for existing user
+// 				return res.status(400).json({ error: existingUserError.message })
+// 			}
+
+// 			if (existingUser) {
+// 				// User with the same username already exists
+// 				// Decide whether to update existing user or return an error
+// 				// For example, to update the existing user:
+// 				const { error: updateError } = await supabase
+// 					.from('users')
+// 					.update({
+// 						accessToken: userCredentials.access_token,
+// 						// ... other fields to update
+// 					})
+// 					.eq('username', userCredentials.data.username)
+
+// 				if (updateError) {
+// 					return res.status(400).json({ error: updateError.message })
+// 				}
+
+// 				return res.status(200).json(userCredentials)
+// 			} else {
+// 				// No user with the same username found, create new user
+// 				const { data, error } = await supabase.from('users').insert({
+// 					id: userCredentials.data.id,
+// 					accessToken: userCredentials.access_token,
+// 					username: userCredentials.data.username,
+// 				})
+
+// 				if (error) {
+// 					return res.status(400).json({ error })
+// 				}
+
+// 				return res.status(200).json(userCredentials)
+// 			}
+// 		}
+// 	} catch (err) {
+// 		return res.status(400).json({ err })
+// 	}
+// }
